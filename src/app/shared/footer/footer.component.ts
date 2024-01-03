6978fa0ed17c3ee350a69ae795b52e6f5231e84b
Hyago <hyago.eurico.nsa@gmail.com>
2024-01-03 11:15:46 -0300

    fix: fixed footer design

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { ContainerComponent } from '../container/container.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [ContainerComponent, MatButtonModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
-export class FooterComponent {
-
-}
+export class FooterComponent {}
