6a2694385850c9a86ae2bf0652dfbdfb5c31b81e
Hyago <hyago.eurico.nsa@gmail.com>
2024-01-04 10:13:49 -0300

    feat: estilização do footer & adição da form busca

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,6 +3,7 @@ import { BannerComponent } from '../../shared/banner/banner.component';
 import { ContainerComponent } from '../../shared/container/container.component';
 import { CardBuscaComponent } from '../../shared/card-busca/card-busca.component';
 import { CardDepoimentoComponent } from '../../shared/card-depoimento/card-depoimento.component';
+import { FormBuscaComponent } from '../../shared/form-busca/form-busca.component';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +13,7 @@ import { CardDepoimentoComponent } from '../../shared/card-depoimento/card-depoi
     ContainerComponent,
     CardBuscaComponent,
     CardDepoimentoComponent,
+    FormBuscaComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
